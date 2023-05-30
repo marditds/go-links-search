b0333e05a9519c3f388f7d82f3306783fc759fc0
marditds <marditds@gmail.com>
2023-05-30 13:27:12 -0700

    color change

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,18 +1,20 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import './Results.css';
 
 export const Results = ({ results }) => {
 
 
     return (
         <div>
-            <ul>
+            <ul className="ps-0">
 
                 {results?.map((res, i) => {
                     return (
-                        <div key={i}>
+                        <div key={i} className='text-start ps-0'>
 
-                            <Link to={`/${res.name}`}>
+                            <Link to={`/${res.name}`} className='text-decoration-none repo--link'
+                                style={{ color: "black" }}>
 
                                 {res?.name}
                             </Link>
